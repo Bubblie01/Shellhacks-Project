39714650d17059097e2da11039dbd6617f683b63
Reece Wilson <rtwboss@gmail.com>
2025-09-27 11:15:48 -0400

    made it work

--- frontend/shellhacks-frontend/src/app/Componets/navbar.tsx
+++ frontend/shellhacks-frontend/src/app/Componets/navbar.tsx
@@ -8,14 +8,14 @@ const Navbar = () => {
             <input id="my-drawer" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content">
                 {/* Page content here */}
-                <label htmlFor="my-drawer" className="btn btn-primary drawer-button">Open drawer</label>
+                <label htmlFor="my-drawer" className="btn btn-primary drawer-button">Pages</label>
             </div>
             <div className="drawer-side">
                 <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
                 <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
                     {/* Sidebar content here */}
-                    <li><a href="../home/page.tsx">Home</a></li>
-                    <li><a href="../profile/page.tsx">Profile</a></li>
+                    <li><a href="../home">Home</a></li>
+                    <li><a href="../profile">Profile</a></li>
                 </ul>
             </div>
         </div>
