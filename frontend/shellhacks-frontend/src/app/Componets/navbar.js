2f8f4474e66dad52cd8118a37b47f94085193337
Reece Wilson <rtwboss@gmail.com>
2025-09-27 02:36:52 -0400

    navbar :3

---
+++ frontend/shellhacks-frontend/src/app/Componets/navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+
+        <div className="drawer">
+            <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content">
+                {/* Page content here */}
+                <label htmlFor="my-drawer" className="btn btn-primary drawer-button">Open drawer</label>
+            </div>
+            <div className="drawer-side">
+                <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+                <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+                    {/* Sidebar content here */}
+                    <li><a href="../home/page.tsx">Home</a></li>
+                    <li><a href="../profile/page.tsx">Profile</a></li>
+                </ul>
+            </div>
+        </div>
+
+    );
+
+};
+
+export default Navbar;
--- frontend/shellhacks-frontend/src/app/display/page.tsx
+++ frontend/shellhacks-frontend/src/app/display/page.tsx
@@ -1,7 +1,11 @@
+import Link from 'next/link';
+import Navbar from '../Componets/navbar';
+
 
 export default function Home() {
   return (
     <div className="">
+        <Navbar />
       <h1>Display Page</h1>
     </div>
   );
