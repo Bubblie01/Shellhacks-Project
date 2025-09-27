3408ae72404e05e894e661031ed240340420aac3
IsaiahKindred <isaiahekindred@gmail.com>
2025-09-27 12:47:02 -0400

    Display page v1

--- frontend/shellhacks-frontend/src/app/display/page.tsx
+++ frontend/shellhacks-frontend/src/app/display/page.tsx
@@ -3,9 +3,13 @@ import Navbar from '../Componets/navbar';
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="h-screen w-screen bg-linear-to-bl from-[#0a014fc3] to-[#3251c3]">
         <Navbar />
-      <h1>Display Page</h1>
+      <div className="mx-[8%] my-[8%] bg-[#00000062] h-[84%] w-[84%] rounded-3xl shadow-2xl">
+        <div className="px-[5%] py-[5%]">
+          <h4>Rohan sux</h4>
+        </div>
+      </div>
     </div>
   );
 }
